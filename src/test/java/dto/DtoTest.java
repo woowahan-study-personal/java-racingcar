cb45f11e930398fefd5275bca7b2c3c5798d028a
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-07-10 15:58:50 +0900

    test(dto): DTO 테스트 목록

---
+++ src/test/java/dto/DtoTest.java
@@ -0,0 +1,18 @@
+package dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DtoTest {
+    @Test
+    @DisplayName("1. 차량 객체에 대한 DTO 테스트")
+    void carDtoTest() {
+
+    }
+
+    @Test
+    @DisplayName("2. 차량 목록에 대한 DTO 테스트")
+    void carRepositoryDtoTest() {
+
+    }
+}
