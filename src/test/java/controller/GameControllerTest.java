5efddefb203704980694bc2a594e66e75da1a6f6
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-07-10 15:56:29 +0900

    test(controller): 컨트롤러 테스트 목록

---
+++ src/test/java/controller/GameControllerTest.java
@@ -0,0 +1,28 @@
+package controller;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GameControllerTest {
+
+    @Test
+    @DisplayName("1. 컨트롤러를 통한 게임 1턴 수행 요청")
+    void gameControllerRunTurnTest() {
+
+    }
+
+    @Test
+    @DisplayName("2. 컨트롤러를 통한 현재의 차량 정보 요청")
+    void getCarRepositoryInfoRequestTest() {
+
+    }
+
+    @Test
+    @DisplayName("3. 컨트롤러를 통한 게임 결과 반환 요청")
+    void gameResultRequestTest() {
+
+    }
+}
