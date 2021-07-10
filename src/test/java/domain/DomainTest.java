d2844962a18b70243fe2c45db4bef126b64979ce
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-07-10 15:21:54 +0900

    test(domain): 도메인 테스트 목록 작성

---
+++ src/test/java/domain/DomainTest.java
@@ -0,0 +1,58 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DomainTest {
+
+    @Test
+    @DisplayName("1. Car 도메인 생성 성공")
+    void carCreateSuccessTest() {
+        
+    }
+
+    @Test
+    @DisplayName("2. Car 도메인 생성 실패")
+    void carCreateFailTest() {
+
+    }
+
+    @Test
+    @DisplayName("3. Car 도메인 getter 테스트")
+    void carGetterTest() {
+
+    }
+
+    @Test
+    @DisplayName("4. Car 도메인 이동 테스트")
+    void carMoveTest() {
+
+    }
+
+    @Test
+    @DisplayName("5. CarRepository 도메인 생성 및 내용 추가 성공")
+    void carRepositoryCreateSuccessTest() {
+
+    }
+
+    @Test
+    @DisplayName("6. CarRepository 도메인 내용 추가 실패")
+    void carRepositoryCreateFailTest() {
+
+    }
+
+    @Test
+    @DisplayName("7. CarRepository 도메인 getCarList ")
+    void carRepositoryGetCarListTest() {
+
+    }
+
+    @Test
+    @DisplayName("8. CarRepository 도메인 각 Car 객체의 이동 명령")
+    void carRepositoryCarMoveList() {
+
+    }
+}
