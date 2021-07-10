1f7c7fc203ffb4e75d615154cc85f5ca8007861e
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-07-10 17:10:50 +0900

    test(view): view 테스트 목록

---
+++ src/test/java/view/OutputViewTest.java
@@ -0,0 +1,20 @@
+package view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OutputViewTest {
+
+    @Test
+    @DisplayName("1. 차량 상태에 대한 출력 테스트")
+    void printCarStatusTest() {
+
+    }
+
+    @Test
+    @DisplayName("2. 게임 결과에 대한 출력 테스트")
+    void printGameResultTest() {
+
+    }
+
+}
