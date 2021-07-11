de0c7336f5bec4d6ee7111bce98b8d2b740ebcd8
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-07-11 23:26:15 +0900

    feat(service): GameService

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,6 +1,5 @@
 package racingcar.service;
 
-import java.util.Collections;
 import racingcar.domain.Car;
 import racingcar.domain.CarRepository;
 import racingcar.tool.GameRandomTool;
@@ -10,7 +9,7 @@ import java.util.List;
 
 public class GameService {
 
-    private static int minimumRequirementNumberForCarMove = 4;
+    private static int MINIMUM_REQUIREMENT_NUMBER_FOR_CAR_MOVE = 4;
 
     private CarRepository carRepository;
     private int longestDistanceInMatch = 0;
@@ -21,36 +20,50 @@ public class GameService {
         carRepository = CarRepository.createGameByCarNameArray(carNames);
     }
 
+    public CarRepository getCarRepository() {
+        return carRepository;
+    }
+
     private int getLongestDistanceInMatch() {
         return this.longestDistanceInMatch;
     }
 
     private boolean isCarMoveAble(int number) {
-        return number >= minimumRequirementNumberForCarMove;
+        return number >= MINIMUM_REQUIREMENT_NUMBER_FOR_CAR_MOVE;
     }
 
-    public void updateLongestDistance() {
+    public void runCarByRandomNumber(Car car) {
+        if (isCarMoveAble(GameRandomTool.makeGameRandomNumber())) {
+            car.moveCar();
+        }
+    }
+
+    public void runOneTurn() {
+        for (Car car : carRepository.getCarList()) {
+            runCarByRandomNumber(car);
+        }
+    }
+
+    private void updateLongestDistance() {
         for (Car car : carRepository.getCarList()) {
             this.longestDistanceInMatch = Math
                 .max(car.getPosition(), getLongestDistanceInMatch());
         }
     }
 
-    public void runOneTurn() {
-        for (Car car : carRepository.getCarList()) {
-            if (isCarMoveAble(GameRandomTool.makeGameRandomNumber())) {
-                car.moveCar();
-            }
+    private void applyResultInArray(List<String> result, Car car) {
+        if (car.getPosition() == getLongestDistanceInMatch()) {
+            result.add(car.getName());
         }
     }
 
     public List<String> makeWinnerResult() {
         List<String> result = new ArrayList<>();
 
+        updateLongestDistance();
+
         for (Car car : carRepository.getCarList()) {
-            if (car.getPosition() == getLongestDistanceInMatch()) {
-                result.add(car.getName());
-            }
+            applyResultInArray(result, car);
         }
 
         return result;
