bea1f25e42b94f8220df1b95639a4857acf4beba
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-07-12 00:01:20 +0900

    feat(racingcar): Application

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,33 +1,35 @@
 package racingcar;
 
 import java.util.Scanner;
-import racingcar.service.GameService;
+import racingcar.view.GameView;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
 
-    private static void runGame(Scanner sc) {
-//        String[] carNameArray = InputView.inputCarList(sc);
-//        GameService game = GameService.createGameByCarNameArray(carNameArray);
-//        int turnCount = InputView.inputTurn(sc);
-//
-//        OutputView.printExecutionResultUIMessage();
-//        OutputView.printEmptyNextLine();
-//
-//        while (turnCount != 0) {
-//            turnCount--;
-//            game.runOneTurn();
-//            game.updateLongestDistance();
-//            OutputView.printCurrentGameTurnStatus(game);
-//        }
-//
-//        OutputView.printMatchWinner(game);
+    private static void runGame(String[] carNameArray, int turnCount) {
+        GameView gameView = new GameView(carNameArray);
+
+        while (turnCount != 0) {
+            turnCount--;
+            gameView.runTurnRequest();
+        }
+
+        OutputView.printMatchWinner(gameView.gameResultRequest());
+    }
+
+    private static void runGameRequest(Scanner sc) {
+        String[] carNameArray = InputView.inputCarList(sc);
+        int turnCount = InputView.inputTurn(sc);
+
+        OutputView.printExecutionResultUIMessage();
+        OutputView.printEmptyNextLine();
+
+        runGame(carNameArray, turnCount);
     }
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-
-//        runGame(scanner);
+        runGameRequest(scanner);
     }
 }
