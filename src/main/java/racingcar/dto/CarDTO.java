fbbd581673f61020161bf9b5e6981044c77f2659
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-07-11 23:13:06 +0900

    feat(dto): CarDTO, CarRepositoryDTO

--- src/main/java/racingcar/dto/CarDTO.java
+++ src/main/java/racingcar/dto/CarDTO.java
@@ -1,5 +1,25 @@
 package racingcar.dto;
 
+import racingcar.domain.Car;
+
 public class CarDTO {
 
+    private String name;
+    private int position;
+
+    private CarDTO() {
+    }
+
+    public CarDTO(Car Car) {
+        name = Car.getName();
+        position = Car.getPosition();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
--- src/main/java/racingcar/dto/CarRepositoryDTO.java
+++ src/main/java/racingcar/dto/CarRepositoryDTO.java
@@ -1,5 +1,22 @@
 package racingcar.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.CarRepository;
+
 public class CarRepositoryDTO {
 
+    private CarRepositoryDTO() {
+    }
+
+    public static List<CarDTO> getCarDTOList(CarRepository carRepository) {
+        List<CarDTO> carDTOList = new ArrayList<>();
+
+        for (Car car : carRepository.getCarList()) {
+            carDTOList.add(new CarDTO(car));
+        }
+
+        return carDTOList;
+    }
 }
