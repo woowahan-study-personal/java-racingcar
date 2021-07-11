c5afcb16b358da5e211986894f952827e9ce9a92
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-07-11 23:35:43 +0900

    feat(controller): GameController

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,5 +1,30 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.dto.CarDTO;
+import racingcar.dto.CarRepositoryDTO;
+import racingcar.service.GameService;
+
 public class GameController {
 
+    GameService gameService;
+
+    private GameController() {
+    }
+
+    public GameController(String[] carNameArray) {
+        this.gameService = new GameService(carNameArray);
+    }
+
+    public void runTurn() {
+        gameService.runOneTurn();
+    }
+
+    public List<CarDTO> getCarListInfo() {
+        return CarRepositoryDTO.getCarDTOList(gameService.getCarRepository());
+    }
+
+    public List<String> getGameResultList() {
+        return gameService.makeWinnerResult();
+    }
 }
